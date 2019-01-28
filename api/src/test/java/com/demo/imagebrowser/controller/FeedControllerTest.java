ed8a5057233ee264eba6d2eee8cbba91b442cea5
Bora Bilgin <bora@bilg.in>
2019-01-28 09:11:10 -0500

    Added Controller tests

---
+++ api/src/test/java/com/demo/imagebrowser/controller/FeedControllerTest.java
@@ -0,0 +1,53 @@
+package com.demo.imagebrowser.controller;
+
+import com.demo.imagebrowser.domain.Feed;
+import com.demo.imagebrowser.domain.FeedCategory;
+import com.demo.imagebrowser.service.FeedService;
+import com.demo.imagebrowser.service.impl.UserDetailsService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(FeedController.class)
+public class FeedControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @MockBean
+    private FeedService feedService;
+
+    @MockBean
+    private UserDetailsService userDetailsService;
+
+    @Test
+    public void shouldReturnJSON_whenRequestedAllFeeds() throws Exception {
+        FeedCategory category = new FeedCategory("test");
+        List<Feed> feeds = Arrays.asList(new Feed("test", "test", category));
+
+        given(feedService.getAll()).willReturn(feeds);
+
+        mvc.perform(get("/feed")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$[0].name").value(feeds.get(0).getName()));
+
+    }
+}
