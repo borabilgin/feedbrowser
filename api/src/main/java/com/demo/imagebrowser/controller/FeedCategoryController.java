87047a694f30224a40dc0370b7f15b1ea763b499
Bora Bilgin <bora@bilg.in>
2019-02-01 00:43:49 -0500

    Turn on security for addCategory method

--- api/src/main/java/com/demo/imagebrowser/controller/FeedCategoryController.java
+++ api/src/main/java/com/demo/imagebrowser/controller/FeedCategoryController.java
@@ -5,6 +5,7 @@ import com.demo.imagebrowser.service.FeedService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,6 +19,7 @@ public class FeedCategoryController {
     private FeedService feedService;
 
     @RequestMapping(method = RequestMethod.PUT)
+    @PreAuthorize("hasAuthority('ADMIN')")
     public ResponseEntity<FeedCategory> addCategory(@RequestParam() String categoryName) {
         FeedCategory category = feedService.addCategory(categoryName);
         return new ResponseEntity<>(category, HttpStatus.OK);
